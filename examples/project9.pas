7115d3dd4c845efa5262d193e672752319c8dcb9
Tunç Bahçecioğlu <tunc.bahcecioglu@gmail.com>
2025-09-06 21:22:57 +0200

    update example code

--- examples/project9.pas
+++ examples/project9.pas
@@ -2,6 +2,18 @@ program Project9;
 
 uses
   System.SysUtils;
+
+function Sum(a, b: integer): integer;
+begin
+  Result := a + b;
+end;
+
+procedure Foo;
+begin
+  Writeln('Foo');
+end;
+
+
 var
   LDouble: double;
 begin
@@ -10,4 +22,13 @@ begin
 
   if not LBool then
     EXIT(1);
+
+  for var LIndex := 0 to 10 do
+  begin
+    LDouble := Sum(LIndex, LIndex * 2) * LValue / 3.0;
+    Writeln(LDouble:0:2);
+    if LIndex = 5 then
+      Break;
+
+  end;
 end.
