bf9340d60fb9df52ebe36e113a3f318f5811eaa4
Tunç Bahçecioğlu <tunc.bahcecioglu@gmail.com>
2025-09-06 16:13:32 +0200

    add tests for unit name replacement

---
+++ test-data/unit_name_fix/ex1.correct.test.pas
@@ -0,0 +1,54 @@
+unit ex1;
+
+interface
+
+uses
+  b,
+  c,
+  f,
+  f,
+  g,
+  h,
+  k,
+  l,
+  t,
+  u,
+  y;
+
+implementation
+uses
+  b,
+  c,
+  f,
+  f,
+  g,
+  h,
+  k,
+  l,
+  t,
+  u,
+  y;
+
+uses
+  MemoryGuard,
+  Optional,
+  ProjectDB.DelphiFacade.FacadeManagedDataView,
+  ProjectDB.DelphiFacade.FacadeManagedDataViewFactory,
+  ProjectDB.DelphiFacade.FMDMeshEntityComponentData,
+  ProjectDB.DelphiFacade.FMDMeshEntityResultData,
+  ProjectDB.DelphiFacade.FMDNodeCoordinates,
+  ProjectDB.DelphiFacade.FMOMesh,
+  ProjectDB.DelphiFacade.FMOMeshEntity,
+  ProjectDB.DelphiFacade.FMOMeshEntityPhasesState,
+  ProjectDB.DelphiFacade.FMOPhase,
+  ProjectDB.DelphiFacade.FMOStep,
+  System.Generics.Collections,
+  System.SysUtils,
+  Xvtkwc.DataArray,
+  Xvtkwc.Definitions,
+  Xvtkwc.DllLoader,
+  Xvtkwc.Piece,
+  Xvtkwc.PvdItem,
+  Xvtkwc.Writer;
+
+end.
---
+++ test-data/unit_name_fix/ex1.original.test.pas
@@ -0,0 +1,40 @@
+unit ex1;
+
+interface
+
+uses
+  b,
+  f,
+  c,u,y,t,g,h,f,
+  k,l;
+
+implementation
+uses
+  b,
+  f,
+  c,u,y,t,g,h,f,
+  k,l;
+
+uses
+ProjectDB.DelphiFacade.FMDMeshEntityResultData,
+ProjectDB.DelphiFacade.FMDNodeCoordinates,
+ProjectDB.DelphiFacade.FMOMesh,
+ProjectDB.DelphiFacade.FMOMeshEntity,
+ProjectDB.DelphiFacade.FMOMeshEntityPhasesState,
+ProjectDB.DelphiFacade.FMOPhase,
+ProjectDB.DelphiFacade.FMOStep,
+ProjectDB.DelphiFacade.FacadeManagedDataView,
+ProjectDB.DelphiFacade.FacadeManagedDataViewFactory,
+SysUtils,
+Xvtkwc.DataArray,
+Xvtkwc.Definitions,
+Xvtkwc.DllLoader,
+Generics.Collections,
+MemoryGuard,
+Optional,
+ProjectDB.DelphiFacade.FMDMeshEntityComponentData,
+Xvtkwc.Piece,
+Xvtkwc.PvdItem,
+Xvtkwc.Writer;
+
+end.
