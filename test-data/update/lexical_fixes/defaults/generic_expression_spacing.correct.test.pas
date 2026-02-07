4b2d0c4ed86ad88a084ce52732090397030cc975
Tunç Bahçecioğlu <tunc.bahcecioglu@gmail.com>
2026-02-07 18:43:56 +0100

    add more generic tests

--- test-data/update/lexical_fixes/defaults/generic_expression_spacing.correct.test.pas
+++ test-data/update/lexical_fixes/defaults/generic_expression_spacing.correct.test.pas
@@ -8,6 +8,13 @@ type
     procedure Foo();
   end;
 
+function ProcessSteps(
+  const AStepNumbers: TArray<TStepNr>;
+  const ASteps: TArray<TStepData>
+): TArray2<TStructure>;
+
+function LookupStructure(const AStructureType: TStructureType): TOptional<TStructure>;
+
 implementation
 
 procedure TMy.Foo();
@@ -16,6 +23,19 @@ var
 begin
   LObjects := TList<PObject>.Create();
   LObjects := TDictionary<String, TList<Integer>>.Create();
+end;
+
+function ProcessSteps(
+  const AStepNumbers: TArray<TStepNr>;
+  const ASteps: TArray<TStepData>
+): TArray2<TStructure>;
+begin
+  Result := TArray2<TStructure>;
+end;
+
+function LookupStructure(const AStructureType: TStructureType): TOptional<TStructure>;
+begin
+  Result := Default(TOptional<TStructure>);
 end;
 
 end.
--- test-data/update/lexical_fixes/defaults/generic_unary_spacing.original.test.pas
+++ test-data/update/lexical_fixes/defaults/generic_unary_spacing.original.test.pas
@@ -9,6 +9,13 @@ type
 var
   X: Integer;
 
+function ProcessSteps(
+  const AStepNumbers: TArray<TStepNr>;
+  const ASteps: TArray<TStepData>
+): TArray2 < TStructure >;
+
+function LookupStructure(const AStructureType: TStructureType): TOptional<TStructure>;
+
 implementation
 
 function Add(a, b: Integer): Integer;
@@ -20,6 +27,19 @@ begin
   X := + Foo;
   X := a * - 2;
   Result := a - b;
+end;
+
+function ProcessSteps(
+  const AStepNumbers: TArray<TStepNr>;
+  const ASteps: TArray<TStepData>
+): TArray2 < TStructure >;
+begin
+  Result := TArray2<TStructure>;
+end;
+
+function LookupStructure(const AStructureType: TStructureType): TOptional<TStructure>;
+begin
+  Result := Default(TOptional<TStructure>);
 end;
 
 end.
